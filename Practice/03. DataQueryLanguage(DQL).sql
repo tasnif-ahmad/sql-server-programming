e84d56873ff7e9252b452e9a8b26515b275794a5
Tasnif <tasnif.hbtw@gmail.com>
2026-01-06 12:07:03 +0530

    Data Query Language( DQL) changes.

--- Practice/03. DataQueryLanguage(DQL).sql
+++ Practice/03. DataQueryLanguage(DQL).sql
@@ -1,12 +1,35 @@
-﻿SELECT * FROM Products;
+﻿-- Display all details of all products
+SELECT * FROM Products;
 
+-- Display product name and price only
+SELECT ProductName, Price 
+FROM Products;
+
+-- Display products whose price is greater than 50
+SELECT * 
+FROM Products
+WHERE Price > 50;
+
+-- Display top 5 products
+SELECT TOP 5 * 
+FROM Products;
+
+-- Display all details of all shippers
 SELECT * FROM Shippers;
 
+-- Display shippers ordered alphabetically by name
+SELECT * 
+FROM Shippers
+ORDER BY ShipperName;
+
+-- Display all details of all students
 SELECT * FROM Students;
 
+-- Display students who belong to the BCA course
+SELECT * 
+FROM Students
+WHERE Course = 'BCA';
 
-
-
-
-
-
+-- Display total number of students
+SELECT COUNT(*) AS TotalStudents
+FROM Students;
